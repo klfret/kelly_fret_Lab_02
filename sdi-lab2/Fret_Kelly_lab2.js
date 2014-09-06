eea2d078334a3a6bffe433f0bf8f687dd1a097bc
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-06 16:27:58 -0400

    Pushing my Lab 2 Assignment

---
+++ sdi-lab2/Fret_Kelly_lab2.js
@@ -0,0 +1,38 @@
+//alert("JavaScript works! Lab 2");
+
+
+// *************
+// Kelly L. Fret
+// SDI 1409
+// Lab 2
+// *************
+
+
+// Start of code
+
+// My variable declarations
+var myBikeRideStartPoint = "County Lake's Gate";				//String data type
+var myBikeRideEndPoint   = "County Lake's \"Picnic Area\"";		//String data type with Escape Sequence
+var totalMilesOfRide     = "10";								//Number data type
+var sunnyDay             = false;								//Boolean data type
+
+
+// My confirm
+sunnyDay = confirm("Was it a sunny day during the bike ride? \nClick OK for Yes, Cancel for No.");
+
+
+// My Prompt
+totalMilesOfRide = parseFloat(prompt("How many miles did you bike ride?"));
+
+
+// My Outputs
+console.log("I went on a " + totalMilesOfRide + "-mile bike ride with other riders.");
+console.log("Our starting point was at the " + myBikeRideStartPoint + ".");
+console.log("And our ending point was at the " + myBikeRideEndPoint + ".");
+console.log("It's " + sunnyDay + " that it was a sunny day during the bike ride. ");
+
+
+// To confirm the parseFloat is working
+totalMilesOfRide = totalMilesOfRide + 10;
+console.log(totalMilesOfRide);
+// End of code
